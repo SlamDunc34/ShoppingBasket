a38b9974c088b15e037b9c34848241c19c409beb
Duncan Henderson <duncanroberthenderson@gmail.com>
2017-05-07 23:20:03 +0100

    added product class with getters for name and price

---
+++ Product.java
@@ -0,0 +1,17 @@
+public class Product {
+
+    private String name;
+    private Integer price;
+
+    public Product(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Integer getPrice() {
+        return this.price;
+    }
